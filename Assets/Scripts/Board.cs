0e4acf746ccac73bee29390fb672253ad3b90948
herval <hervalfreire@gmail.com>
2021-01-17 12:23:56 -0800

    WIP fantasy set with animations

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,18 +8,14 @@ public class Board : MonoBehaviour
     public int width = 10;
     public int height = 10; // 10 as deault so a single tile has a scale of 0.1
 
-    public GameObject tilePrefab;
-
+    public TileFactory tileFactory;
     public PieceFactory pieceFactory;
-
-    public Color darkTiles;
-    public Color clearTiles;
 
     Tile[,] tiles;
 
     public void AddPiece(Piece.Type piece, int x, int y, Player player)
     {
-        Piece p = pieceFactory.Create(tiles[x,y], piece, player.facingUp, player);
+        Piece p = pieceFactory.Create(tiles[x, y], piece, player.facingUp, player);
         player.Pieces.Add(p);
 
         p.transform.position = tiles[x, y].transform.position;
@@ -35,7 +31,8 @@ public class Board : MonoBehaviour
         opponent.Pieces.ForEach(p => p.ResetMoves());
 
         // compute all movement vectors of the _opponent_ pieces
-        opponent.Pieces.ForEach((p) => {
+        opponent.Pieces.ForEach((p) =>
+        {
             p.ComputeMoves(tiles);
         });
 
@@ -51,8 +48,9 @@ public class Board : MonoBehaviour
 
         // compute all the movement vectors of current player's pieces
         // the king CANNOT move to a threatened tile
-        currentPlayer.Pieces.ForEach(p => {
-        p.ComputeMoves(tiles);
+        currentPlayer.Pieces.ForEach(p =>
+        {
+            p.ComputeMoves(tiles);
 
             // le roi is a little snowflake
             if (p.isKing)
@@ -96,7 +94,7 @@ public class Board : MonoBehaviour
                     .FindAll(t => t.CurrentPiece?.player == currentPlayer)
                     .ConvertAll(t => t.CurrentPiece); // grab all player pieces blocking the move
 
-                if(blockingPieces.Count == 1) // only restrict movement if there's only ONE piece defending the king
+                if (blockingPieces.Count == 1) // only restrict movement if there's only ONE piece defending the king
                 {
                     blockingPieces[0].PotentialMoves = blockingPieces[0].PotentialMoves.FindAll(m => vector.Contains(m.Tile));
                 }
@@ -105,66 +103,7 @@ public class Board : MonoBehaviour
 
     public void Reset()
     {
-        Color nextColor;
-
-        tiles = new Tile[width, height];
-
-        var totalSize = this.transform.GetComponent<SpriteRenderer>().bounds.size;
-        float tileSizeX = totalSize.x / ((float)width);
-        float tileSizeY = totalSize.y / ((float)height);
-
-        // half a board offset since coords are in the center
-        float offsetX = totalSize.x / 2f;
-        float offsetY = totalSize.y / 2f;
-
-        // offset the tiles half a tile to the right, taking the new scale in consideration, based on the actual width x height of the board
-        var tileScale = tilePrefab.transform.localScale;
-        var tileSize = tilePrefab.GetComponent<SpriteRenderer>().bounds.size;
-        float newTileScaleX = 1f / width;
-        float newTileScaleY = 1f / height;
-
-        // if the board was always 8x8, we wouldn't need to rescale - this computes the new size so we can render in the right place
-        float tileOffsetX = ((tileSize.x / tileScale.x) * newTileScaleX) / 2f;
-        float tileOffsetY = ((tileSize.y / tileScale.y) * newTileScaleY) / 2f;
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                // render locations relative to the scale of the tiles
-                // TODO use the size of the board as bounds instead
-
-                float px = (((float)x) * tileSizeX) - offsetX + tileOffsetX;
-                float py = ((((float)y) + 1f) * tileSizeY) - offsetY + tileOffsetY; // this makes ZERO SENSE
-
-                // scale up tiles to fit
-                var tileObj = Instantiate(tilePrefab,
-                    new Vector3(px, py, 0f), // offset rendering by half a board + half a tile, since coords start on the center
-                    Quaternion.identity, this.transform);
-                tileObj.GetComponent<SpriteRenderer>().transform.localScale = new Vector3(newTileScaleX, newTileScaleY, 0f); // reescale for reals
-
-
-                // if the square is double odd or double even, it‘s Dark (diagonals)
-                if ((x+y) % 2 != 0)
-                {
-                    nextColor = darkTiles;
-                }
-                else
-                {
-                    nextColor = clearTiles;
-                }
-
-                // alternate colors
-                tiles[x, y] = tileObj.GetComponent<Tile>();
-                tiles[x, y].Color = nextColor;
-
-                tiles[x, y].x = x;
-                tiles[x, y].y = y;
-            }
-        }
-
-        // if we dont do this, the prototype tile on the corner will be clickable :fliptable
-        tilePrefab.GetComponent<BoxCollider>().enabled = false;
+        this.tiles = tileFactory.Reset(this);
     }
 
     internal Tile[,] Tiles()
@@ -179,10 +118,10 @@ public class Board : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(
-            new Vector3(0, 0, 0),
-            new Vector3(width * tilePrefab.transform.localScale.x, height * tilePrefab.transform.localScale.y, 0)
-        );
+        //Gizmos.DrawWireCube(
+        //    new Vector3(0, 0, 0),
+        //    new Vector3(width * tilePrefab.transform.localScale.x, height * tilePrefab.transform.localScale.y, 0)
+        //);
 
         // TODO draw the board wire
         //float tw= width / tilePrefab.transform.localScale.x;
