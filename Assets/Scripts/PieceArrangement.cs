25f767fcfbc727581067658e678b54145a49d77d
herval <hervalfreire@gmail.com>
2021-02-14 19:47:04 -0800

    HORDE MODE

--- Assets/Scripts/PieceArrangement.cs
+++ Assets/Scripts/PieceArrangement.cs
@@ -3,42 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PieceArrangement {
-    public abstract void Position(PieceFactory factory, TileView[,] boardView, ref Piece[,] pieces, PlayerView player);
-}
-
-// a classic game of chess
-public class StandardPieceArrangement : PieceArrangement {
-    PieceType[,] Arrangement = {
-        {
-            PieceType.Rook, PieceType.Knight, PieceType.Bishop, PieceType.Queen, PieceType.King, PieceType.Bishop,
-            PieceType.Knight, PieceType.Rook
-        }, {
-            PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn,
-            PieceType.Pawn, PieceType.Pawn
-        },
-    };
-
-    // Start is called before the first frame update
-    public override void Position(
+    protected abstract PieceType?[,] Arrangement();
+    
+    public void Position(
         PieceFactory factory,
         TileView[,] boardView,
         ref Piece[,] pieces,
         PlayerView player
     ) {
+        var arrangement = Arrangement();
         if (player.StartingPosition == PlayerPosition.Top) {
             // render top pieces, they always belong to player facing down
-            for (int y = 0; y < Arrangement.GetLength(0); y++) {
-                for (int x = 0; x < Arrangement.GetLength(1); x++) {
-                    pieces[x, boardView.GetLength(1) - y - 1] = AddPiece(factory, Arrangement[y, x],
-                        boardView[x, boardView.GetLength(1) - y - 1], player);
+            for (int y = 0; y < arrangement.GetLength(0); y++) {
+                for (int x = 0; x < arrangement.GetLength(1); x++) {
+                    if (arrangement[y, x] != null) {
+                        pieces[x, boardView.GetLength(1) - y - 1] = AddPiece(factory, arrangement[y, x].GetValueOrDefault(),
+                            boardView[x, boardView.GetLength(1) - y - 1], player);
+                    }
                 }
             }
         }
         else {
             // render bottom pieces
-            for (int y = 0; y < Arrangement.GetLength(0); y++) {
-                for (int x = 0; x < Arrangement.GetLength(1); x++) {
-                    pieces[x, y] = AddPiece(factory, Arrangement[y, x], boardView[x, y], player);
+            for (int y = 0; y < arrangement.GetLength(0); y++) {
+                for (int x = 0; x < arrangement.GetLength(1); x++) {
+                    if (arrangement[y, x] != null) {
+                        pieces[x, y] = AddPiece(factory, arrangement[y, x].GetValueOrDefault(), boardView[x, y], player);
+                    }
                 }
             }
         }
@@ -52,5 +43,46 @@ public class StandardPieceArrangement : PieceArrangement {
         p.transform.position = tile.transform.position;
 
         return p.State;
+    }
+}
+
+public class HordeArrangement : PieceArrangement {
+    PieceType?[,] arrangement = {
+        {
+            PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn,
+            PieceType.Pawn, PieceType.Pawn
+        }, {
+            PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn,
+            PieceType.Pawn, PieceType.Pawn
+        }, {
+            PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn,
+            PieceType.Pawn, PieceType.Pawn
+        },
+        {
+            null, null, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn,
+            null, null
+        }
+     };
+    
+
+    protected override PieceType?[,] Arrangement() {
+        return arrangement;
+    }
+}
+
+// a classic game of chess
+public class StandardPieceArrangement : PieceArrangement {
+    PieceType?[,] arrangement = {
+        {
+            PieceType.Rook, PieceType.Knight, PieceType.Bishop, PieceType.Queen, PieceType.King, PieceType.Bishop,
+            PieceType.Knight, PieceType.Rook
+        }, {
+            PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn, PieceType.Pawn,
+            PieceType.Pawn, PieceType.Pawn
+        },
+    };
+
+    protected override PieceType?[,] Arrangement() {
+        return arrangement;
     }
 }
